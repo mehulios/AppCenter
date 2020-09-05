cdedf19939054ff7011c7e766991ed825c4199f3
Mehul Makwana <mehul.makwana@tatadigital.com>
2020-09-05 17:26:09 +0530

    Initial Commit

---
+++ AppCenterCICD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppCenterCICD
+//
+//  Created by Mehul Makwana on 05/09/20.
+//  Copyright © 2020 Mehul Makwana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
